8cf30ca0c38be4d97683ba92a4604590279d7b4c
Pedro Ferreira <ilzogoiby@gmail.com>
2013-05-10 19:33:51 +0200

    Fixed YouTube regexp issue
     * closes #1

--- ext/youtube.js
+++ ext/youtube.js
@@ -1,5 +1,6 @@
 var request = require('request'),
     utils = require('../lib/utils'),
+    querystring = require('querystring'),
     Deferred = require("promised-io/promise").Deferred;
 
 
@@ -19,9 +20,12 @@ function get_video_info(vid, options) {
 }
 
 function process(url, info, bot, promise) {
-  var m = url.match(/^(?:(?:https?\:\/\/(?:www\.)?youtube\.com\/watch\?v=)|(?:https?\:\/\/youtu\.be\/))(.*)/);
+  var m = url.match(/^(?:(?:https?\:\/\/(?:www\.)?youtube\.com\/watch\?(.+))|(?:https?\:\/\/youtu\.be\/(.+)))\#.*/);
+
   if (m) {
-    get_video_info(m[1], bot.config.imgur).then(function(result) {
+    var video_id = m[1] ? querystring.parse(m[1]).v : m[2];
+
+    get_video_info(video_id, bot.config.imgur).then(function(result) {
       info.youtube = result.entry;
       var title = result.entry.title.$t;
 
